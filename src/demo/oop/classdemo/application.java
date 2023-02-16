ed73d7790b85617ca54165ecf29d145b0e199660
qhc <xiongdiquan@163.com>
2023-02-17 00:09:11 +0800

    this night learn class again.

---
+++ src/demo/oop/classdemo/application.java
@@ -0,0 +1,18 @@
+package demo.oop.classdemo;
+
+public class application {
+    public static void main(String[] args) {
+        person wangwu = new person();
+        wangwu.age=3;
+        wangwu.name= "steve";
+        wangwu.sex="man";
+        System.out.println(wangwu.a());
+
+        person zhaoshan = new person();
+        zhaoshan.name="zhaosan";
+        System.out.println(zhaoshan.name);
+
+
+
+    }
+}
---
+++ src/demo/oop/classdemo/person.java
@@ -0,0 +1,13 @@
+package demo.oop.classdemo;
+
+public class person {
+   String name;
+   int age;
+   String sex;
+
+
+   public  String a(){
+       return (name + age + sex);
+   }
+
+}
