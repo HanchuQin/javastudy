ee11561750510d488ee24209fc84483d930af289
qhc <xiongdiquan@163.com>
2023-02-17 23:05:45 +0800

    学习封装，属性私有 get/set 高内聚，低耦合。 一般通过set方法做判断进行有效性校验。 快捷键 alt+insert

---
+++ src/demo/oop/classdemo/student.java
@@ -0,0 +1,34 @@
+package demo.oop.classdemo;
+
+public class student {
+    private int xuehao;
+    private String name;
+    int age;
+
+    public int getXuehao() {
+        return xuehao;
+    }
+
+    public void setXuehao(int xuehao) {
+        this.xuehao = xuehao;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<120 || age>0){
+            this.age = age;
+        }else this.age=0;
+
+    }
+}
