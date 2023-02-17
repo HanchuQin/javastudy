690303857da94cf4693b37efcdbce8736d2c3526
qhc <xiongdiquan@163.com>
2023-02-17 22:43:39 +0800

    构造器学习，默认无参构造，当创建形参构造时必须存在无参构造。快捷键alt+insert

--- src/demo/oop/classdemo/person.java
+++ src/demo/oop/classdemo/person.java
@@ -6,8 +6,12 @@ public class person {
    String sex;
 
 
-   public  String a(){
-       return (name + age + sex);
+   public person() {
    }
 
+   public person(String name, int age, String sex) {
+      this.name = name;
+      this.age = age;
+      this.sex = sex;
+   }
 }
