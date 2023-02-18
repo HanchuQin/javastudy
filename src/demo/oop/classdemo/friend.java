67e1f3cf47897e52746f875b53efde50ca45dbfc
K <shengju403@sctz.com>
2023-02-18 16:55:45 +0800

    提交了一个继承。但是父类没有定义好，需要重新思考其定义。

---
+++ src/demo/oop/classdemo/friend.java
@@ -0,0 +1,16 @@
+package demo.oop.classdemo;
+
+/**
+ * @ClassName friend
+ * @Description TODO
+ * @Author gmh
+ * @Date 2023/2/18 16:45
+ * @Version 1.0
+ */
+
+//练习继承，只有静态方法能被继承。
+public class friend extends person {
+
+
+
+}
